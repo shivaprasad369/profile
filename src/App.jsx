ca6d7ca01e8d5b7b4f6379f2945a71d306b39e37
shivaprasad369 <122955366+shivaprasad369@users.noreply.github.com>
2024-04-27 14:08:10 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-import Button from './Components/Button'
-import ButtonGradient from './assets/svg/ButtonGradient'
+
 import Navbar from './Components/Navbar'
 import Home from './Components/home'
 import About from './Components/About'
